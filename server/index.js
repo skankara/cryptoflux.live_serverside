6a78a9ba3b73b79c3281fd25688c1234709f3de1
skankara <skankara@iu.edu>
2020-04-12 23:16:46 -0400

    Code to include google oAuth #1

--- server/index.js
+++ server/index.js
@@ -1,19 +1,11 @@
 const express = require('express');
-// const authRoutes = require('./routes/authRoutes');
-// require('./models/User');
-// require('./services/passport');
-// const keys = require('./config/keys');
+const authRoutes = require('./routes/authRoutes');
+require('./services/passport');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// authRoutes(app);
-
-// mongoose.connect(keys.mongoURI);
+authRoutes(app);
 
 app.listen(PORT,()=>{
   console.log("Server started at http://localhost:5000");
 });
-
-//API Key AIzaSyB99_W1nzB0junYmRCP55WLsivpg0cg0WU
-//Client Id - 30334964036-igm187dm11ri6oopdc2kop81rep4ig6e.apps.googleusercontent.com
-//Client Secret - WPAN4Hjy02gT5AwYbHR8KEpe
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+    /**
+     * Router routing back to this path when returned from gogole
+     */  
+    app.get('/auth/google', passport.authenticate('google',{
+        scope: ['profile', 'email']
+    }) 
+    );
+    /**
+     * Callback router and will take us back to passport.use google strategy
+     */
+    app.get('/auth/google/callback', passport.authenticate('google'));
+}
